a43904eece5c1aa005efabbf8df475036a8f2394
Elnur21 <elnurme@code.edu.az>
2023-04-07 03:45:42 +0400

    Add project files.

---
+++ Final/Enums/Enums.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final.Enums
+{
+    public enum Enums: byte
+    {
+        AuthorAdd,
+        AuthorEdit,
+        AuthorDelete,
+        AuthorGetAll,
+        AuthorGetByID,
+        AuthorGetByName,
+
+        BookAdd,
+        BookEdit,
+        BookDelete,
+        BookGetAll,
+        BookGetByID,
+        BookGetByName,
+
+
+        SaveAndExit
+    }
+}
